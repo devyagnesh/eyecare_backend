57859992e8adccba0dbc9737d034f506a499f034
devyagnesh <con.boringmacho@gmail.com>
2025-11-13 22:01:18 +0530

    fix: Remove admin login links from email verification views
    - Removed dashboard link from logo in all verification views
    - Changed page titles from 'Admin Panel' to 'Eyecare'
    - Logo is now non-clickable (static image only)
    - Kept 'Go to Sign In' button linking to regular login page

--- resources/views/auth/verify-email-success.blade.php
+++ resources/views/auth/verify-email-success.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Email Verified | Admin Panel</title>
+    <title>Email Verified | Eyecare</title>
     <link rel="icon" href="{{ asset('assets/images/brand-logos/favicon.ico') }}" type="image/x-icon">
     <script src="{{ asset('assets/js/authentication-main.js') }}"></script>
     <link id="style" href="{{ asset('assets/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -23,9 +23,7 @@
         <div class="row justify-content-center authentication authentication-basic align-items-center h-100">
             <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-6 col-sm-8 col-12">
                 <div class="mb-3 d-flex justify-content-center auth-logo">
-                    <a href="{{ route('dashboard') }}">
-                        <img src="{{ asset('assets/images/brand-logos/desktop-dark.png') }}" alt="logo" class="desktop-dark">
-                    </a>
+                    <img src="{{ asset('assets/images/brand-logos/desktop-dark.png') }}" alt="logo" class="desktop-dark">
                 </div>
                 <div class="card custom-card my-4 border z-3 position-relative">
                     <div class="card-body p-0">
